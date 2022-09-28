3eeb81f33aa8c9c5f04dd98fd412429a6e635ae7
orgilshdeee <67452021+orgilshdeee@users.noreply.github.com>
2022-09-28 12:37:26 +0800

    push

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv'
+import mongoose from "mongoose";
 
+dotenv.config();
 
-async function bootstrap() {
+const PORT = process.env.PORT || 4000;
+const ATLAS_MONGO_SERVER = process.env.ATLAS_MONGO_CONNECTION || "localhost";
+
+let server:any;
+
+mongoose.connect(ATLAS_MONGO_SERVER).then(()=>{
+  console.log("Connected to the MongoDB");
+  bootstrap(server);
+})
+
+async function bootstrap(server:any) {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  server = await app.listen(PORT,()=>{
+    console.log(`Server is runnon on : ${PORT}`)
+  });
 }
-bootstrap();
+
---
+++ fe/pages/components/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () =>{
+    return <>I'm footer nigga</>
+}
+
+export default Footer;
