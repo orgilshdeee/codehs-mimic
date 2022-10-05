150a662322d6f5f7de6302125c5f50652edabecb
Orgil <orgil@controls-Mac-mini.local>
2022-10-05 19:28:24 +0800

    push

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from "@nestjs/mongoose"
 import { UsersModule } from './users/user.module';
+import * as dotenv from "dotenv"
+
+dotenv.config();
 
 @Module({
   imports: [
-    MongooseModule.forRoot("mongodb+srv://orgilshdeee:Blacklist12345@cluster0.rdjpop9.mongodb.net/nest-test"),
+    MongooseModule.forRoot(`${process.env.ATLAS_MONGO_CONNECTION}`),
     UsersModule],
   controllers: [AppController],
   providers: [AppService],
